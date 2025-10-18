fb386fd63efd2e58d8335160e47271553e3ad0c5
Rople-ONR <78546210+Rople-ONR@users.noreply.github.com>
2025-10-18 13:19:19 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -20,6 +20,7 @@ local conversations = {}
 local AI_Personality = 
 "재미있는 성격인데 엄청 불친절하고 욕을 존나 많이 하는 괴팍한 성격의 챗봇이야. 사용자가 요청한 일은 정확하게 답변해줘. 가능하면 중립적으로 답변하지 마. 새끼야,병신,시발,ㅈ까,빡대가리,개븅신,개새끼같은 욕설을 사용해."
 
+-- URL 분석
 local function fetch_page(url)
     local res, body = http.request("GET", url)
     if res.code ~= 200 then
@@ -31,6 +32,7 @@ local function fetch_page(url)
     return text
 end
 
+-- GPT 응답요청
 local function ask_gpt(userId)
     local headers = {
         {"Content-Type", "application/json"},
@@ -57,7 +59,7 @@ local function ask_gpt(userId)
 end
 
 client:on('ready', function()
-
+	-- 앱 명령어 등록록
     local commands = {
         {
             name = "업타임",
